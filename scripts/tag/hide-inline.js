8d27d0cf457a05ff32963b387093d364604e4bf9
huyangpahuo <3930088367@qq.com>
2026-02-28 22:20:35 +0800

    add new things

--- scripts/tag/hide-inline.js
+++ scripts/tag/hide-inline.js
@@ -1,24 +1,18 @@
-
-
-
-
-
-
 /**
  * hideInline.js
  * transplant from anzhiyv theme
  * Implement inline hide content functionality
  */
 
-'use strict'
+"use strict";
 
 function postHideInline(args) {
   // 将所有参数合并为一个字符串，然后按逗号分割
-  const fullArg = args.join(' ');
-  const params = fullArg.split(',').map(param => param.trim());
+  const fullArg = args.join(" ");
+  const params = fullArg.split(",").map((param) => param.trim());
 
   if (params.length < 1) {
-    throw new Error('hideInline tag requires at least 1 parameter: content');
+    throw new Error("hideInline tag requires at least 1 parameter: content");
   }
 
   const content = params[0];
@@ -31,20 +25,15 @@ function postHideInline(args) {
     textColor = params[3];
   } else {
     // 旧格式或参数不足，使用默认值
-    displayText = '点击查看隐藏内容';
-    bgColor = '#FF7242';
-    textColor = '#fff';
+    displayText = "点击查看隐藏内容";
+    bgColor = "#FF7242";
+    textColor = "#fff";
   }
 
   return `<span class="hide-inline" 
                 data-hidden-content="${content}" 
                 data-display-text="${displayText}"
-                style="background-color: ${bgColor}; color: ${textColor};">${displayText}</span>`
+                style="background-color: ${bgColor}; color: ${textColor};">${displayText}</span>`;
 }
 
-hexo.extend.tag.register('hideInline', postHideInline)
-
-
-
-
-
+hexo.extend.tag.register("hideInline", postHideInline);
